42c020cd8706d70241cdc31e2d794cad3ddfbf90
JeHee <ljh945999@gmail.com>
2025-03-02 17:20:11 +0900

    fix : validation logic

---
+++ src/auth/adapter/out.persistence/auth.authInfoAdapter.ts
@@ -0,0 +1,15 @@
+import { AuthLoadAuthInfo } from '../../application/port/out/auth.loadAuthInfo';
+import { AuthSaveAuthInfo } from '../../application/port/out/auth.saveAuthInfo';
+import { RedisRepository } from './auth.redis.repository';
+import { Injectable } from '@nestjs/common';
+@Injectable()
+export class AuthAuthInfoAdapter implements AuthLoadAuthInfo, AuthSaveAuthInfo {
+  constructor(private readonly redisRepository: RedisRepository) {}
+
+  setExInfo = (key: string, value: string, time: number): void => {
+    this.redisRepository.setEx(key, value, time);
+  };
+  getInfo(key: string) {
+    return this.redisRepository.get(key);
+  }
+}
